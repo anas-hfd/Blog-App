5b0d737af519eb0a14e17565297aed7c980ae536
Anas-hfd <Anas1.hfd@gmail.com>
2024-08-15 17:08:04 +0100

    pages and structuring

---
+++ src/pages/ArticlesList.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const ArticlesList = () => {
+  return (
+    <div className='mb-20'>
+        <h1 className='sm:text-4xl text-2xl font-bold my-6'>Welcome to the Article list page!</h1>
+        <p className='mx-auto leading-relaxed text-base mb-4'>lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+        </p>
+        <p className='mx-auto leading-relaxed text-base mb-4'>lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+        </p>
+    </div>
+  )
+}
+
+export default ArticlesList
